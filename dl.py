cd88b6093f737c5b06e037c6da8240b1d3af0f45
sαɴᴊɪᴛ sɪɴʜα <72181891+sanjit-sinha@users.noreply.github.com>
2022-09-13 20:28:02 +0530

    Update dl.py

--- dl.py
+++ dl.py
@@ -1,44 +1,49 @@
-import base64
-import re
 import time
-import requests
-from bs4 import BeautifulSoup
+import requests 
+import re
+import cloudscraper 
+from bs4  import BeautifulSoup
 
 
-url = 'https://psa.pm/exit/d131b9c1f9cb281390cad99fe6b8cb6f/meBhW5x8PH6BemidHPgl4G3uMujdl0cTkOdRu3j3zJDlbsgWMhhKL05gHzsvnx64MSV0WuetpAQdjguU0sgtUhgmrUb0b3i98PCi3usyeiyy9D86gmKk6scex5bPciAM:SGZlajBGRDBVMEpib3F5aU1OaXozZz09:Hfej0FD0U0JboqyiMNiz3mVkZTk3ZDcwZjU1ZjQ2M2I1Mzk2MWRhOGQ0NjRjOGMx'
+def try2link_bypass(url):
+	client = cloudscraper.create_scraper(allow_brotli=False)
+	
+	url = url[:-1] if url[-1] == '/' else url
+	
+	params = (('d', int(time.time()) + (60 * 4)),)
+	r = client.get(url, params=params, headers= {'Referer': 'https://newforex.online/'})
+	
+	soup = BeautifulSoup(r.text, 'html.parser')
+	inputs = soup.find(id="go-link").find_all(name="input")
+	data = { input.get('name'): input.get('value') for input in inputs }	
+	time.sleep(7)
+	
+	headers = {'Host': 'try2link.com', 'X-Requested-With': 'XMLHttpRequest', 'Origin': 'https://try2link.com', 'Referer': url}
+	
+	bypassed_url = client.post('https://try2link.com/links/go', headers=headers,data=data)
+	return bypassed_url.json()["url"]
+		
 
+def try2link_scrape(url):
+	client = cloudscraper.create_scraper(allow_brotli=False)	
+	h = {
+	'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+	}
+	res = client.get(url, cookies={}, headers=h)
+	url = 'https://try2link.com/'+re.findall('try2link\.com\/(.*?) ', res.text)[0]
+	print(try2link_bypass(url))
+    
 
-# --------------------------------------
-
-client = requests.Session()
-h = {
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-}
-res = client.get(url, cookies={}, headers=h)
-
-url = 'https://try2link.com/'+re.findall('try2link\.com\/(.*?) ', res.text)[0]
-
-res = client.head(url)
-
-id = re.findall('d=(.*?)&', res.headers['location'])[0]
-id = base64.b64decode(id).decode('utf-8')
-
-url += f'/?d={id}'
-res = client.get(url)
-
-bs4 = BeautifulSoup(res.content, 'html.parser')
-inputs = bs4.find_all('input')
-data = { input.get('name'): input.get('value') for input in inputs }
-
-time.sleep(6.5)
-res = client.post(
-    'https://try2link.com/links/go',
-    headers={
-        'referer': url,
-        'x-requested-with': 'XMLHttpRequest',
-    }, data=data
-)
-out = res.json()['url'].replace('\/','/')
-
-print(out)
+def psa_bypasser(psa_url):
+	client = cloudscraper.create_scraper(allow_brotli=False)
+	r = client.get(psa_url)
+	soup = BeautifulSoup(r.text, "html.parser").find_all(class_="dropshadowboxes-drop-shadow dropshadowboxes-rounded-corners dropshadowboxes-inside-and-outside-shadow dropshadowboxes-lifted-both dropshadowboxes-effect-default")
+	for link in soup:
+		try:
+			exit_gate = link.a.get("href")
+			try2link_scrape(exit_gate)
+		except: pass
+			
+ 
+ 
+psa_bypasser("https://psa.pm/movie/bubble-2022/")
